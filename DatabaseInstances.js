6ca5b8a518ec302ae4d04c9835c8f08144ccc07b
Niklas Björnberg <niklas.bjornberg@gmail.com>
2015-11-05 17:17:52 +0100

    ehm

--- DatabaseInstances.js
+++ DatabaseInstances.js
@@ -3,9 +3,11 @@
 evalFile("Database.js",this)
 evalFile("Room.js",this)
 evalFile("Region.js",this)
+evalFile("Item.js",this)
 
 var cdb = new Database("characters.data",Character.prototype)
 cdb.SetFactory(CharacterFactory)
 var rgndb = new Database("regions.data",Region.prototype)
 var rdb = new Database("rooms.data",Room.prototype)
+var idb = new Database("items.data"),Item.prototype)
 

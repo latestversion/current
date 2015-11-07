7e710a39c31898d41604da259b7a55c921e7932d
Niklas Björnberg <niklas.bjornberg@gmail.com>
2015-11-07 20:17:42 +0100

    Made array of databases, indexed by type enum

--- DatabaseInstances.js
+++ DatabaseInstances.js
@@ -10,4 +10,16 @@ cdb.SetFactory(CharacterFactory)
 var rgndb = new Database("regions.data",Region.prototype)
 var rdb = new Database("rooms.data",Room.prototype)
 var idb = new Database("items.data",Item.prototype)
+var pdb = new Database("portals.data", Portal.prototype)
+
+
+// This could be in Database.js and be automatically populated by Database ctor
+// Should be removed from global scope, etc.
+var Dbs = []
+
+Dbs[Character.ENUM] = cdb
+Dbs[Region.ENUM] = rgndb
+Dbs[Room.ENUM] = rdb
+Dbs[Item.ENUM] = idb
+Dbs[Portal.ENUM] = pdb
 

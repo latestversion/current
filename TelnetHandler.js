8d609da8506330c74f57ad04fd0abdd571136602
Niklas Björnberg <niklas.bjornberg@gmail.com>
2015-11-17 22:05:10 +0100

    Accepting single \n as input

--- TelnetHandler.js
+++ TelnetHandler.js
@@ -20,12 +20,21 @@ _p.onData = function(d)
 
 _p.parse = function(s)
 {
+	l1("data: " + s.charCodeAt(0) + " " + s.charCodeAt(1))
 	//console.log("TelnetHandler " + this.cid + " got data: " + s)
 	// RFC 1123 3.3.1. states clients should send either \r\0 or \r\n.
+	// (The mac default telnet seems to send 13 10)
 	s = s.replace("\r\0","\n")
 	s = s.replace("\r\n","\n")
 	// Let's safeguard against an odd \r, too.
 	s = s.replace("\r","\n")
+
+	if("\n" == s && 0 == this.buffer.length)
+	{
+		this.lines.push(s)
+		return
+	}
+
 	this.buffer += s
 
 	var nidx = this.buffer.indexOf("\n")
@@ -36,10 +45,10 @@ _p.parse = function(s)
 		this.buffer = remaining
 		if(line)
 		{
-			// replace accidental double spaces etc
-			line = line.replace(/\s+/g," ")
+			// replace accidental double spaces etc (should be moved upwards in the layers)
+			//line = line.replace(/\s+/g," ")
 			// trim ws from right and left
-			line = line.trim()
+			//line = line.trim()
 			this.lines.push(line)
 		}
 		nidx = this.buffer.indexOf("\n")

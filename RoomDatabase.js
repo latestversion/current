0a8cbcdd077b0d79a2c58cacfe16b603fc2aee70
Niklas Björnberg <niklas.bjornberg@gmail.com>
2015-11-28 20:03:37 +0100

    Added RoomFactory bc convenient

--- RoomDatabase.js
+++ RoomDatabase.js
@@ -1,7 +1,11 @@
+evalFile("RoomFactory.js")
+
+
 function RoomDatabase()
 {
   Database.call(this,"rooms.data",Room.prototype)
   this.SetName("RoomDb")
+  this.SetFactory(RoomFactory)
 }
 
 var _p = RoomDatabase.prototype
